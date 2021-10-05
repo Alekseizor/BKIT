e82f22760992d6fc94b444c5edcbf54fdc1e0b73
Alekseizor <74896858+Alekseizor@users.noreply.github.com>
2021-10-05 23:28:53 +0300

    Add files via upload

---
+++ Лабораторная работа №3/lab_python_fp/unique.py
@@ -0,0 +1,36 @@
+import random
+from gen_random import gen_random
+
+
+class Unique(object):
+    def __init__(self, items, **kwargs):
+        self.data = iter(items)
+        self.word = set()
+        if not kwargs:
+            self.app = False
+        else:
+            self.app = kwargs['ignore_case']
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while True:
+            x = next(self.data)
+            if self.app == True and type(x) != int:
+                x = x.lower()
+            if x not in self.word:
+                self.word.add(x)
+                return x
+
+
+def main():
+    data = gen_random(2, 1, 15)
+    #data = ['a', 'A', 'b', 'B', 'a', 'A', 'b', 'B']
+
+    for i in Unique(data, ignore_case=False):
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
